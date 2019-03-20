3b8daeb668080ef4ac5aa99aab5df52023cdbb38
Konstantinos <a16konka@student.his.se>
2019-03-20 10:53:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 
 <?php get_header(); ?>
 
-	<section id="welcome">				
-		<div>					
-			<span class="title">Välkommen till Hyperphoto</span>
-			<p class="textPara">
-	Här kan du boka en professionell fotograf för en evenemang i förväg.<br> 
-	Vår utrustning är bäst i branschen med <br>
-	den högsta kvalitet möjligt.</p>
-		</div> 
-	</section>
-	
+<?php get_template_part('content'); ?>	
+
 <?php get_footer(); ?>
