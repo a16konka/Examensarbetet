fbf5d3ca55d67710c4f8e4c7e8a2a064dc3ef275
Konstantinos <a16konka@student.his.se>
2019-04-22 14:44:01 +0200

    Seed 10

--- Programmeringskod/index.php
+++ Programmeringskod/index.php
@@ -1,33 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>	
-<script type="text/javascript">
-
-    function refreshData(){
-		var searchEngine = 'http://wordpress:8888/';
-		
-		$.ajax({
-	    	url:searchEngine,
-	        type:'GET',
-	        success: results,
-	        error: errormsg
-		});
-	
-		function results(data){
-		   $("#content").html(data);
-		}
-		
-		function errormsg(jqXHR,textStatus,errorThrown){
-		    console.log(jqXHR);
-		    alert("AJAX Error: " + errorThrown);     
-		}
-	}
-
-  </script>
-<body onload="refreshData()">
-	<?php get_header(); ?>
-    <div id="content">Test</div>
-    <?php get_footer(); ?>
-</body>
 
 
-
+<?php get_template_part('content'); ?>	
 
